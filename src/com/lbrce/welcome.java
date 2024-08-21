6d4dd2fbf6f1c345288feea6382210ea8836ee35
AdmiN <AdmiN@ITWS0022>
2024-08-21 14:59:02 +0530

    commit

---
+++ src/com/lbrce/welcome.java
@@ -0,0 +1,10 @@
+package com.lbrce;
+
+public class welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to CICD pipeline");
+	}
+
+}
